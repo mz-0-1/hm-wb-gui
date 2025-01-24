0c249a4fc8ce393a9601aff0b213a20df9ce3542
Matteo Zamparini <matteozamparini@Matteos-MacBook-Air.local>
2025-01-24 14:23:21 -0800

    vercel/nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  productionBrowserSourceMaps: false,
+  swcMinify: true,
+  poweredByHeader: false
 };
 
 export default nextConfig;
